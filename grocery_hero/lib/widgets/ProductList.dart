4bc552a31ccc95f60c787502b9bdd2027d7182ea
whoistahito <taha.amirhs22@gmail.com>
2023-11-19 21:10:10 +0100

    Add Custom List to show all Products in ViewAllProductsScreen

---
+++ grocery_hero/lib/widgets/ProductList.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+
+import 'BuyProductItemCard.dart';
+import 'Product.dart';
+
+class ProductList extends StatelessWidget {
+  final List<Product> products;
+
+  const ProductList({Key? key, required this.products}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      primary: false,
+      scrollDirection: Axis.vertical,
+      itemCount: (products.length / 2).ceil(),
+      // Calculate the number of rows
+      itemBuilder: (context, index) {
+        // Get two products for each row
+        int startIndex = index * 2;
+        int endIndex = startIndex + 1;
+        List<Product> rowProducts = endIndex < products.length
+            ? products.sublist(startIndex, endIndex + 1)
+            : [products[startIndex]];
+
+        return Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: rowProducts
+              .map(
+                (product) => Expanded(
+                  flex: 1,
+                  child: BuyProductItemCard(
+                    price: product.price,
+                    imagePath: product.imagePath,
+                    productName: product.productName,
+                  ),
+                ),
+              )
+              .toList(),
+        );
+      },
+    );
+  }
+}
